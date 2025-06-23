5b0fe91d605b02d2341800434adf48f83a0ef9f2
EdsonDO <2021110367@udh.edu.pe>
2025-06-23 03:43:45 -0500

    Mejora de Layout del Frontend, añadido un Background y varias funciones han sido readaptadas para que funcionen correctamente.

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Frontend';
+   menuColapsado = false;
+
+  constructor(private router: Router) {}
+
+  redireccionar(url: string) {
+    this.router.navigate([url]);
+    console.log(url);
+  }
+
+  toggleMenu() {
+    this.menuColapsado = !this.menuColapsado;
+  }
 }
